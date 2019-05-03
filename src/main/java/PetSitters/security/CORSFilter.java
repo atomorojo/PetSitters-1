0124cc35843f61935fdf26116f632d2228ad66c1
George Bochileanu <georgebochileanu@gmail.com>
2019-05-03 12:06:05 +0200

    Treu uns camps innecessaris
    Treu uns camps innecessaris de l'arxiu application.properties

--- src/main/java/PetSitters/security/CORSFilter.java
+++ src/main/java/PetSitters/security/CORSFilter.java
@@ -28,7 +28,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Methods", "*"); // POST, PUT, GET, OPTIONS, DELETE
         //response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin, Origin, X-Requested-With, Content-Type, Accept, Authorization"); 
+        response.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin, Origin, X-Requested-With, Content-Type, Accept, Authorization");
         response.setHeader("Access-Control-Expose-Headers", "Location");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         filterChain.doFilter(servletRequest, response);
