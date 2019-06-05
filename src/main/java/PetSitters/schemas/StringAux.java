972115214343a2a13b8034a7e76ca27c65a70f6d
Antoni Casas <tonicasas6@gmail.com>
2019-06-05 16:38:00 +0200

    Hotfix

---
+++ src/main/java/PetSitters/schemas/StringAux.java
@@ -0,0 +1,13 @@
+package PetSitters.schemas;
+
+public class StringAux {
+    String thing;
+
+    public String getThing() {
+        return thing;
+    }
+
+    public void setThing(String thing) {
+        this.thing = thing;
+    }
+}
